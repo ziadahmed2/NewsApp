678dc18f3f29c61112023f91a70e7973c9100f21
ziadahmed2 <ziad.96@hotmail.com>
2022-11-24 17:09:30 +0200

    Add Preferences.kt and Base di

---
+++ core/src/main/java/com/itworx/core/domain/model/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.itworx.core.domain.model
+
+data class UserInfo(
+    val country: String,
+    val categories: List<String>
+)
---
+++ core/src/main/java/com/itworx/core/domain/preferences/Preferences.kt
@@ -0,0 +1,20 @@
+package com.itworx.core.domain.preferences
+
+import com.itworx.core.domain.model.UserInfo
+
+
+interface Preferences {
+    fun saveCountry(country: String)
+    fun saveCategories(categories: List<String>)
+
+    fun loadUserInfo(): UserInfo
+
+    fun saveShouldShowOnboarding(shouldShow: Boolean)
+    fun loadShouldShowOnboarding(): Boolean
+
+    companion object {
+        const val KEY_COUNTRY = "country"
+        const val KEY_CATEGORIES = "categories"
+        const val KEY_SHOULD_SHOW_ONBOARDING = "should_show_onboarding"
+    }
+}
